b7fa5397d1b7708cecd0feba6be7c78fcd613dbc
AlexPaladino <paladino.alessio391@gmail.com>
2023-03-18 01:25:34 +0100

    Proporzioni(no-responsive)
    Co-authored-by: Matz2K00 <Matz2K00@users.noreply.github.com>

--- login.php
+++ login.php
@@ -9,22 +9,23 @@
   <?php session_start(); require 'secure/notForLog.php'; ?>
   <link rel="stylesheet" type="text/css" href="css/form.css">
   <script src="js/validateLogin.js"></script>
-  <script src="js/validateEmail.js"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;1,400&display=swap" rel="stylesheet">
 </head>
-<body>
-	<div> 
+<body> 
 	<img loading="lazy" draggable="false" onclick="window.location.href='index.php'" src="img/logo_login.svg" class="logo">
-		<div class="pippo">
-			<p> Non hai un account? <a href="singup.php" id="aRegister">REGISTRATI</a></p>
-		</div>
+	<div class="linkTo">
+		<p> Non hai un account? <a href="singup.php" id="aRegister">Registarati</a></p>
 	</div>
 	<div class="box-form">
-		<a href="#login" id="aLogin" class="active">LOG-IN</a>
-	  <!--/#login.form-action-->
+
+	    <ul class="tabs">
+	    <li>
+	      <a class="active">LOG-IN</a>
+	    </li>
+	  </ul>
 	  <div id="login" class="form-action show">
 	    <form name="loginForm" id="loginForm" onsubmit="return validateLogin()" action="mod_login.php" method="post">
 				<span <?php if(isset($_SESSION['erroL']) && ($_SESSION['erroL'] == "Registrazione effettuata!" || $_SESSION['erroL'] == "Email per recupero password inviata!" || $_SESSION['erroL'] == "Password resettata!")){
@@ -40,16 +41,15 @@
 		    <br>
 		    <input type="email" name="email" placeholder="Inserisci la tua email*" required>
 		    <br>
-		    <input type="password" id="pasw" name="pass" placeholder="Inserisci la password*" required><div id="showPaswL" class="mostra" onclick="showPaswL()">Mostra</div>
+		    <input type="password" id="pasw" name="pass" placeholder="Inserisci la password*" class="pasw" required><div id="showPaswL" class="mostra" onclick="showPaswL()">Mostra</div>
 		    <br>
 				<div id="footerLogin">
-					<input type="checkbox" id="savePasw" name="savePasw" value="savePassword"> <label for="savePasw"> Ricordami</label>
-
-					<a onclick="document.getElementById('popup').style.display='block';" rel="noopener noreferrer" class="dimenticata">Password dimenticata?</a>
+					<input type="checkbox" id="savePasw" name="savePasw" value="savePassword"> <label for="savePasw"> Ricorda la mia password</label>
 					<br>
 					<input type="submit" name="submit" value="ACCEDI" class="button" rel="noopener noreferrer">
 				</div>
-				
+			<a onclick="document.getElementById('popup').style.display='block';" rel="noopener noreferrer" class="dimenticata">Password dimenticata?</a>
+
 			</form>
 			<!-- ----------- POPUP -------------- -->
 					<div id="popup" class="modal">
--- singup.php
+++ singup.php
@@ -6,27 +6,28 @@
 	<title>Form</title>
 	<META HTTP-EQUIV="Pragma" CONTENT="no-cache">
 	<META HTTP-EQUIV="Expires" CONTENT="-1">
-  <?php session_start(); require 'secure/notForLog.php'; ?>
-  <link rel="stylesheet" type="text/css" href="css/form.css">
-  
-  <script src="js/validateRegister.js"></script>
-  <script src="js/validateEmail.js"></script>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
+  	<?php session_start(); require 'secure/notForLog.php'; ?>
+  	<link rel="stylesheet" type="text/css" href="css/form.css">
+  	<script src="js/validateRegister.js"></script>
+  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+  	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;1,400&display=swap" rel="stylesheet">
 </head>
 <body>
-	<div> 
 	<img loading="lazy" draggable="false" onclick="window.location.href='index.php'" src="img/logo_login.svg" class="logo">
-		<div class="pippo"> 
-			<p> Hai già un account? <a href="login.php" id="aRegister">ACCEDI</a></p>
-		</div>
+	<div class="linkTo"> 
+		<p> Hai già un account? <a href="login.php" id="aRegister">Accedi</a></p>
 	</div>
 	<div class="box-form">
-	    <a href="#register" id="aSignup" >SIGN-UP</a>
 
-	<div id="register" class = "form-action show">
+	    <ul class="tabs">
+	    <li>
+	      <a class="active">SIGN-UP</a>
+	    </li>
+	  </ul>
+
+	<div id="register" class="form-action show">
 	    
 	    <form name="signupForm" id="signupForm" onsubmit="return validateRegister()" action="mod_signup.php" method="post">
 				<span <?php if(isset($_SESSION['erroR'])){
@@ -44,18 +45,17 @@
 		    <br>
 		    <input type="email" name="email" placeholder="Email*" required>
 		    <br>
-		    <input type="password" id="paswR" name="pass" placeholder="Password*" required><div id="showPaswR" class="mostra" onclick="showPaswR()">Mostra</div>
+		    <input type="password" id="paswR" name="pass" placeholder="Password*" class="pasw" required><div id="showPaswR" class="mostra" onclick="showPaswR()">Mostra</div>
 		    <br>
-		    <input type="password" id="conf" name="confirm" placeholder="Conferma la password*" required><div id="showConfR" class="mostra" onclick="showConfR()">Mostra</div>
+		    <input type="password" id="conf" name="confirm" placeholder="Conferma la password*" class="pasw" required><div id="showConfR" class="mostra" onclick="showConfR()">Mostra</div>
 		    <br>
 				<div id="footerLogin">
 					<input type="checkbox" id="robot" name="robot" required> <label for="robot"> Non sono un robot</label>
 					<br><br>
-					<input type="checkbox" id="dati" name="dati" required> <label for="dati"> Autorizzo il trattamento dei dati</label>
+					<input type="checkbox" id="dati" name="dati" required> <label for="dati"> Autorizzo al trattamento dei miei dati</label>
 					<br>
 					<input type="submit" name="submit" value="REGISTRATI" class="button reg" rel="noopener noreferrer">
-				</div>
-				
+				</div>	
 			</form>
 	</div>
 	</div>
